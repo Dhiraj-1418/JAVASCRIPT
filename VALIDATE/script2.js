c73e35c40331e672a14dd77bcf2bc4573b084539
Dhiraj Yadav <8475dhirajyadav@gmail.com>
2025-03-26 09:23:52 +0530

    Add files via upload

--- VALIDATE/script2.js
+++ VALIDATE/script2.js
@@ -1,12 +1,26 @@
-let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let check = "Rahul8";
-let get;
-let count;
+
+function nameValidateCheck(){
+  let array = "0123456789";
+let check = "asd";
+let nameValidate = [];
 for (i = 0; i < check.length; i++) {
-  count = 0;
   for (j = 0; j < array.length; j++) {
-    check[i] == array[j];
-    count++;
+    if (check[i] == array[j]) {
+      // nameValidate.push(check[i]);
+   
+      return "Enter valid name";
+    }
+    else
+    {
+      return "";
+    }
+    
   }
 }
-document.writeln(count);
+
+}
+
+
+
+let x=nameValidateCheck();
+document.writeln(x)
