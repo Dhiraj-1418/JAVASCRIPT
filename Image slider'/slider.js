331338927142ea9e90fbf5b20e84c7748e111a17
Dhiraj Yadav <8475dhirajyadav@gmail.com>
2025-04-09 11:37:14 +0530

    Add files via upload

---
+++ Image slider'/slider.js
@@ -0,0 +1,11 @@
+const images = ["test.jpeg",
+    "https://cdn.pixabay.com/photo/2021/09/13/21/06/floral-background-6622475_960_720.png",
+    "https://cdn.pixabay.com/photo/2020/07/28/11/43/circles-5444818_1280.png",
+    "https://cdn.pixabay.com/photo/2023/08/02/18/21/monstera-8165765_1280.jpg"
+
+];
+const left = document.getElementById("slideLeft");
+const right = document.getElementById("slideRight");
+const image = document.getElementsByClassName("images")
+let index = 0;
+ img.src=images[index];
